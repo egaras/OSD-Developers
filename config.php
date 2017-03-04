fb84519e1886c0159034fcc1846a3d1f3eac8554
yassermohsen <yassermohsen101010@gmail.com>
2017-03-04 17:10:57 +0200

    DB manager class

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+include 'DBManager.php';
+define("DB_HOST", "localhost");
+define("DB_USER", "yasser");
+define("DB_PASS", "");
+define("DB_NAME", "simpleBlog");
+
+$DB = new DBManager();
+?>
