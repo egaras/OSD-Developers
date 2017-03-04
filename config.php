2647dd7ea5f4707b1e36f95ff38b07cfacaef971
EhabGamal <ehab.g.sh@gmail.com>
2017-03-04 14:51:30 +0200

    Added config file with DB configs and gitignore file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ehab
+ * Date: 3/4/17
+ * Time: 2:36 PM
+ */
+return [
+    'database'=>[
+        'host'=>'172.0.0.1',
+        'name'=>'osddevelopers',
+        'username'=>'root',
+        'password'=>'root',
+        'connection'=>'mysql:host=172.0.0.1;',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'options'=>[
+            PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
