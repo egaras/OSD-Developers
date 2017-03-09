ed686d3b90f2c9bf95cc7ec7d20d333e6499a935
yassermohsen <yassermohsen101010@gmail.com>
2017-03-09 03:17:27 +0200

    add sending email feature

--- config.php
+++ config.php
@@ -17,8 +17,8 @@ return [
     'database'=>[
         'host'=>'172.0.0.1',
         'name'=>'osddevelopers',
-        'username'=>'root',
-        'password'=>'root',
+        'username'=>'yasser',
+        'password'=>'',
         'connection' => 'mysql:host=127.0.0.1',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
---
+++ controllers/sendEmail.php
@@ -0,0 +1,83 @@
+<?php
+require '../vendor/autoload.php';
+require '../core/bootstrap.php';
+
+// error_reporting(-1);
+// ini_set('display_errors', 'On');
+// set_error_handler("var_dump");
+//
+// // Special mail settings that can make mail less likely to be considered spam
+// // and offers logging in case of technical difficulties.
+//
+// ini_set("mail.log", "/tmp/mail.log");
+// ini_set("mail.add_x_header", TRUE);
+// $headers = implode("\r\n", [
+//     'From: yassermohsen101010@gmail.com',
+//     'Reply-To: webmaster@example.com',
+//     'X-Mailer: PHP/' . PHP_VERSION
+// ]);
+// die(var_dump(mail('yassermohsen101010@gmail.com','theSubject','hellooooo',$headers)));
+// mail('yassermohsen101010@gmail.com','the subject','hellooooo',$headers);
+
+// function send_mail($email,$subject,$msg) {
+//    //$api_key="pubkey-eae3f4d051f4ca95eaeb20d5fed59112";/* Api Key got from https://mailgun.com/cp/my_account */
+//    $api_key="key-6ec6c261975fd4feaad7c980ca6c75f5";
+//    //$domain ="sandboxe04ae10162fe4188961c3a20c3da9970.mailgun.org";/* Domain Name you given to Mailgun */
+//    $domain ="yasser.mydomain.com";
+//    $ch = curl_init();
+//    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+//    curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$api_key);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+//    curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v3/'.$domain.'/messages');
+//    curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+//     'from' => 'Open <mail@youriste.com>',
+//     'to' => $email,
+//     'subject' => $subject,
+//     'html' => $msg
+//    ));
+//    $result = curl_exec($ch);
+//    curl_close($ch);
+//    return $result;
+// }
+// var_dump(send_mail("ehab.g.sh@gmail.com", "Hello", "hii"));
+// die(var_dump(1));
+
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
+//require 'vendor/autoload.php';
+// If you are not using Composer
+require("../sendgrid-php/sendgrid-php.php");
+$request_body = json_decode('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "yassermohsen101010@gmail.com"
+        }
+      ],
+      "subject": "Hello man!"
+    }
+  ],
+  "from": {
+    "email": "hell@osd-developers.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "visit facebook http://www.facebook.com"
+    }
+  ]
+}');
+
+//$apiKey = getenv('SENDGRID_API_KEY');
+$apiKey = "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";
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
+//echo $response->headers();
+//die(var_dump(1));
+?>
