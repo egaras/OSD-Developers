de62de81411d40d08dfcf1ed315d2ee8b1988bb4
yassermohsen <yassermohsen101010@gmail.com>
2017-03-10 22:35:37 +0200

    add loggedin and admin verification function

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,9 +6,23 @@
  * Time: 5:39 PM
  */
 //require 'vendor/autoload.php';
-
+session_start();
 App::bind('config', require $_SERVER['DOCUMENT_ROOT']."/OSD-Developers/config.php");
 App::bind('database', new QueryBuilder(
     Connection::open(App::get('config')['database'])
 ));
 $db = App::get('database');
+
+function isLoggedIn(){
+  if(isset($_SESSION['userId'])){
+      return true;
+  }
+  return false;
+}
+
+function isAdmin(){
+  if($user->roleid == 0){
+    return true;
+  }
+  return false;
+}
