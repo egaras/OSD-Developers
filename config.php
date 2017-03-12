374a8348408b9bee72dc55b964b57ae44aa59fd6
yassermohsen <yassermohsen101010@gmail.com>
2017-03-12 16:10:15 +0200

    update config

--- config.php
+++ config.php
@@ -13,32 +13,32 @@
  * Date: 3/4/17
  * Time: 2:36 PM
  */
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-return [
-    'database'=>[
-        'name'=>'heroku_eb8c565d31ce2cb',
-        'username'=>'b54dd2bdf3fbad',
-        'password'=>'d6a19bf0',
-        'connection' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;',
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'options'=>[
-            PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
-        ]
-    ]
-];
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// return [
+//     'database'=>[
+//         'name'=>'heroku_eb8c565d31ce2cb',
+//         'username'=>'b54dd2bdf3fbad',
+//         'password'=>'d6a19bf0',
+//         'connection' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;',
+//         'charset' => 'utf8',
+//         'collation' => 'utf8_unicode_ci',
+//         'options'=>[
+//             PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+//         ]
+//     ]
+// ];
 
-//return [
-//    'database'=>[
-//        'host'=>'127.0.0.1',
-//        'name'=>'osddevelopers',
-//        'username'=>'root',
-//        'password'=>'root',
-//        'connection' => 'mysql:host=127.0.0.1',
-//        'charset' => 'utf8',
-//        'collation' => 'utf8_unicode_ci',
-//        'options'=>[
-//            PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
-//        ]
-//    ]
-//];
+return [
+   'database'=>[
+       'host'=>'172.30.55.85',
+       'name'=>'osddevelopers',
+       'username'=>'yasser',
+       'password'=>'yasser',
+       'connection' => 'mysql:host=172.30.55.85',
+       'charset' => 'utf8',
+       'collation' => 'utf8_unicode_ci',
+       'options'=>[
+           PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+       ]
+   ]
+];
